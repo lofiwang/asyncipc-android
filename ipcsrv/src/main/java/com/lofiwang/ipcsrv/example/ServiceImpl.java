77055ee59d65bf30481748f6742b500a99789be3
lofiwang <lofiwang.coder@gmail.com>
2018-11-21 00:35:22 +0800

    add pools

--- ipcsrv/src/main/java/com/lofiwang/ipcsrv/example/ServiceImpl.java
+++ ipcsrv/src/main/java/com/lofiwang/ipcsrv/example/ServiceImpl.java
@@ -4,10 +4,21 @@ import android.os.Message;
 import android.os.Messenger;
 
 import com.lofiwang.ipcsrv.base.MsgPoolService;
+import com.lofiwang.ipcsrv.thread.ExecutorHandler;
+
+import java.lang.ref.WeakReference;
 
 
 public class ServiceImpl extends MsgPoolService {
     private static final String TAG = "NewsService";
+    WeakReference<ExecutorHandler> weakReference = new WeakReference<>(new ExecutorHandler());
+    private static ExecutorHandler executorHandler = new ExecutorHandler() {
+        @Override
+        protected void onHandleMessage(Message msg) {
+            super.onHandleMessage(msg);
+        }
+    };
+
 
     public ServiceImpl() {
         super(TAG);
---
+++ ipcsrv/src/main/java/com/lofiwang/ipcsrv/util/Pools.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lofiwang.ipcsrv.util;
+
+/**
+ * public class MyPooledClass {
+ * <p>
+ * private static final SynchronizedPool<MyPooledClass> sPool =
+ * new SynchronizedPool<MyPooledClass>(10);
+ * <p>
+ * public static MyPooledClass obtain() {
+ * MyPooledClass instance = sPool.acquire();
+ * return (instance != null) ? instance : new MyPooledClass();
+ * }
+ * <p>
+ * public void recycle() {
+ * // Clear state if needed.
+ * sPool.release(this);
+ * }
+ * }
+ */
+public final class Pools {
+
+    public static interface Pool<T> {
+        public T acquire();
+
+        public boolean release(T instance);
+    }
+
+    private Pools() {
+    }
+
+    public static class SimplePool<T> implements Pool<T> {
+        private final Object[] mPool;
+
+        private int mPoolSize;
+
+        public SimplePool(int maxPoolSize) {
+            if (maxPoolSize <= 0) {
+                throw new IllegalArgumentException("The max pool size must be > 0");
+            }
+            mPool = new Object[maxPoolSize];
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public T acquire() {
+            if (mPoolSize > 0) {
+                final int lastPooledIndex = mPoolSize - 1;
+                T instance = (T) mPool[lastPooledIndex];
+                mPool[lastPooledIndex] = null;
+                mPoolSize--;
+                return instance;
+            }
+            return null;
+        }
+
+        @Override
+        public boolean release(T instance) {
+            if (isInPool(instance)) {
+                throw new IllegalStateException("Already in the pool!");
+            }
+            if (mPoolSize < mPool.length) {
+                mPool[mPoolSize] = instance;
+                mPoolSize++;
+                return true;
+            }
+            return false;
+        }
+
+        private boolean isInPool(T instance) {
+            for (int i = 0; i < mPoolSize; i++) {
+                if (mPool[i] == instance) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public static class SynchronizedPool<T> extends SimplePool<T> {
+        private final Object mLock = new Object();
+
+        public SynchronizedPool(int maxPoolSize) {
+            super(maxPoolSize);
+        }
+
+        @Override
+        public T acquire() {
+            synchronized (mLock) {
+                return super.acquire();
+            }
+        }
+
+        @Override
+        public boolean release(T element) {
+            synchronized (mLock) {
+                return super.release(element);
+            }
+        }
+    }
+}
